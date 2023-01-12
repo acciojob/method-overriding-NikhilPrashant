ac73a429068b5544888aeb0c50b7188e3c93b60c
nikhilprashant <nikhilprashant1998@gmail.com>
2023-01-12 22:14:42 +0530

    Solved again and merged again twice to check error again 2

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -6,6 +6,7 @@ public class Main {
         }
     }
     public static class B extends A {
+        @Override
         public String meth() {
             return "Method is overridden in Extended class B";
         }
@@ -13,7 +14,6 @@ public class Main {
     public static void main(String[] args) {
         B obj = new B();
         System.out.println(obj.meth()); // Cannot call method of A as it gets Overridden for B
-        B obj1 = new B();
         System.out.println(obj1.meth()); // method has been overridden by B
     }
 }
